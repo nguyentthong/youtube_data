0fa8613105cc45538fc72a3e8317728e1bcffde6
Thong Thanh Nguyen <thongnguyen050999@gmail.com>
2023-12-03 01:21:52 +0800

    Update download_videos_extract_keyframes.py

--- download_videos_extract_keyframes.py
+++ download_videos_extract_keyframes.py
@@ -7,11 +7,11 @@ import numpy as np
 import multiprocessing as mp
 from collections import defaultdict
 
-
 downloaded_video_set = set()
 if os.path.exists("checked_to_download_video_list.txt"):
     with open("checked_to_download_video_list.txt", 'r') as f: checked_to_download_video_list = f.readlines()
     downloaded_video_set = set([checked_to_download_video.strip() for checked_to_download_video in checked_to_download_video_list])
+
 
 if os.path.exists("downloaded_videos"):
     downloaded_video_list = os.listdir("downloaded_videos")
@@ -21,19 +21,19 @@ if os.path.exists("downloaded_videos"):
                 f.write(video + '\n')
                 downloaded_video_set.add(video)
 
-
 checked_to_extract_keyframe_video_list = []
 if os.path.exists("checked_to_extract_keyframe_video_list.txt"):
     with open("checked_to_extract_keyframe_video_list.txt", 'r') as f: checked_to_extract_keyframe_video_list = f.readlines()
 processed_video_set = set([video.strip() for video in checked_to_extract_keyframe_video_list])
 
+
 if os.path.exists("video_frames"):
     video_frames = os.listdir("video_frames")
     with open("checked_to_extract_keyframe_video_list.txt", 'a') as f:
         for video in video_frames:
-            if video not in processed_video_set:
-                f.write(video + '\n')
-                processed_video_set.add(video)
+            if video + ".mp4" not in processed_video_set:
+                f.write(video + ".mp4" + '\n')
+                processed_video_set.add(video + ".mp4")
 
 
 def download_video(url):
@@ -70,10 +70,7 @@ def extract_keyframes(url):
 
         video_frame_list = sorted(os.listdir(os.path.join("temp", video_id)), key=lambda x: int(x.split('.')[0].split('_')[1]))
         if len(video_frame_list) < n:
-            shutil.rmtree(os.path.join("temp", video_id))
-            filename = os.path.join("downloaded_videos", video_id + '.mp4')
-            os.remove(filename)
-
+            pass
         else:
             os.makedirs(os.path.join("video_frames", video_id), exist_ok=True)
             selected_frame_idx_set = set(np.linspace(1, len(video_frame_list)-1, n).astype(int))
@@ -113,11 +110,11 @@ def main():
     os.makedirs("temp", exist_ok=True)
     os.makedirs("video_frames", exist_ok=True)
 
-
     num_frame_range_url_dict = defaultdict(list)
     for i in trange(len(url_list)):
-        if url_list[i].split('=')[-1] in processed_video_set: 
-            shutil.rmtree(os.path.join("temp", video_id))
+        if url_list[i].split('=')[-1] + ".mp4" in processed_video_set:
+            video_id = url_list[i].split('=')[-1]
+            shutil.rmtree(os.path.join("temp", video_id), ignore_errors=True)
             filename = os.path.join("downloaded_videos", video_id + '.mp4')
             try:
                 os.remove(filename)
@@ -129,9 +126,9 @@ def main():
         num_frame_range_url_dict[chunk_index].append(url_list[i])
 
     chunk_index_list = sorted(list(num_frame_range_url_dict.keys()))
-
     for i in trange(len(chunk_index_list)):
-        current_to_process_url_list = num_frame_range_url_dict[i]
+        chunk_index = chunk_index_list[i]
+        current_to_process_url_list = num_frame_range_url_dict[chunk_index]
 
         to_download_video_list = []
         for url in current_to_process_url_list:
